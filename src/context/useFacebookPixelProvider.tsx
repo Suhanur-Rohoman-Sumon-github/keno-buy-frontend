966e19c1a9864f40022aedd5d627a223ee5399bf
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2025-07-18 21:59:33 +0600

    pixel added

--- src/context/useFacebookPixelProvider.tsx
+++ src/context/useFacebookPixelProvider.tsx
@@ -19,7 +19,7 @@ const FacebookPixelProvider = ({ children }: Props) => {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', 'YOUR_PIXEL_ID'); 
+          fbq('init', '1144848137478287'); 
           fbq('track', 'PageView');
         `}
       </Script>
