1b282546409a63c4209c3cdc408560eed2abbe63
Suhnaur-Rohoman-Sumon-github <sumonsuhanurrohoman@gmail.com>
2025-07-18 21:46:47 +0600

    pixel added

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -4,6 +4,7 @@ import * as React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import UserProvider from "@/context/useProvider";
+import FacebookPixelProvider from "@/context/useFacebookPixelProvider";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -13,10 +14,12 @@ const queryClient = new QueryClient();
 export function Providers({ children }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <UserProvider>
-        <Toaster richColors />
-        {children}
-      </UserProvider>
+      <FacebookPixelProvider>
+        <UserProvider>
+          <Toaster richColors />
+          {children}
+        </UserProvider>
+      </FacebookPixelProvider>
     </QueryClientProvider>
   );
 }
